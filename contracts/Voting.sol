9f9e6522e5c95c068a5ef8c45aa0f48554931ec5
Степан Инин <inin_01@mail.ru>
2022-03-22 13:16:31 +0700

    feature function addCanditate

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -3,6 +3,27 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Voting {
   constructor()  {
+    owner = msg.sender;
+  }
 
+  address public owner;
+  uint private Count;
+
+  struct Candidate {
+    uint id;
+    string name;
+    uint totalVotes;
+  }
+
+  mapping(uint => Candidate) public candidates;
+
+  modifier requireOwner() {
+    require(owner == msg.sender, "No access");
+    _;
+  }
+
+  function addCandidate(string memory new_name) public requireOwner {
+    Count ++;
+    candidates[Count] = Candidate(Count, new_name, 0);
   }
 }
